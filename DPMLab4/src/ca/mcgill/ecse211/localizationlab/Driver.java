9e1f8c6ee71208bd1b78d03ca0dcffca561faaed
LaoHuang-xX <xu.hai@mail.mcgill.ca>
2017-10-08 09:19:54 -0400

    Added a basic moveforward method without requiring distance

--- DPMLab4/src/ca/mcgill/ecse211/localizationlab/Driver.java
+++ DPMLab4/src/ca/mcgill/ecse211/localizationlab/Driver.java
@@ -8,97 +8,164 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 // Taken from the last lab.
 
 /**
- * Class that handles moving the robot when moving to a waypoint. Delegates control to the
+ * 
+ * Class that handles moving the robot when moving to a waypoint. Delegates
+ * control to the
+ * 
  * PController when avoiding an obstacle.
+ * 
+ * 
  * 
  * @author Justin Tremblay
  *
+ * 
+ * 
  */
+
 public class Driver {
-  private static final int FORWARD_SPEED = 200;
-  private static final int ROTATE_SPEED = 75;
 
-  // The PController won't be necessary for this lab.
-  // PController pCont;
+	private static final int FORWARD_SPEED = 200;
 
-  EV3LargeRegulatedMotor leftMotor;
-  EV3LargeRegulatedMotor rightMotor;
-  double leftRadius, rightRadius;
-  double width;
+	private static final int ROTATE_SPEED = 75;
 
-  boolean rotating = false;
-  boolean moving_forward = false;
+	// The PController won't be necessary for this lab.
 
-  public Driver(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
-      double leftRadius, double rightRadius, double width) {
-    this.leftMotor = leftMotor;
-    this.rightMotor = rightMotor;
-    this.leftRadius = leftRadius;
-    this.rightRadius = rightRadius;
-    this.width = width;
+	// PController pCont;
 
-    // pCont = new PController(leftMotor, rightMotor, 30, 2);
+	EV3LargeRegulatedMotor leftMotor;
 
-    leftMotor.setAcceleration(3000);
-    rightMotor.setAcceleration(3000);
-  }
+	EV3LargeRegulatedMotor rightMotor;
 
-  public void rotate(double angle, boolean use_degrees, boolean ret) {
-    leftMotor.setAcceleration(1000);
-    rightMotor.setAcceleration(1000);
-    leftMotor.setSpeed(ROTATE_SPEED);
-    rightMotor.setSpeed(ROTATE_SPEED);
+	double leftRadius, rightRadius;
 
-    if (!use_degrees) {
-      angle = Math.toDegrees(angle);
-    }
+	double width;
 
-    if (angle < 0) {
-      // turn right
-      leftMotor.rotate(convertAngle(leftRadius, width, Math.abs(angle)), true);
-      rightMotor.rotate(-convertAngle(rightRadius, width, Math.abs(angle)), false || ret);
-    } else {
-      // turn left
+	boolean rotating = false;
 
-      leftMotor.rotate(-convertAngle(leftRadius, width, Math.abs(angle)), true);
-      rightMotor.rotate(convertAngle(rightRadius, width, Math.abs(angle)), false || ret);
-    }
-  }
+	boolean moving_forward = false;
 
-  public void moveTo(double dist, boolean ret) {
-    leftMotor.setAcceleration(3000);
-    rightMotor.setAcceleration(3000);
-    leftMotor.setSpeed(FORWARD_SPEED);
-    rightMotor.setSpeed(FORWARD_SPEED);
+	public Driver(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
 
+			double leftRadius, double rightRadius, double width) {
 
+		this.leftMotor = leftMotor;
 
-    leftMotor.rotate(convertDistance(leftRadius, dist), true);
-    rightMotor.rotate(convertDistance(rightRadius, dist), false || ret);
-  }
+		this.rightMotor = rightMotor;
 
-  public void stop() {
-    rightMotor.stop();
-    leftMotor.stop();
-  }
+		this.leftRadius = leftRadius;
 
-  public void avoidObstacle(float dist) {
-    // pCont.processUSData(dist);
-  }
+		this.rightRadius = rightRadius;
 
-  private static int convertDistance(double radius, double distance) {
-    return (int) ((180.0 * distance) / (Math.PI * radius));
-  }
+		this.width = width;
 
-  private static int convertAngle(double radius, double width, double angle) {
-    return convertDistance(radius, Math.PI * width * angle / 360.0);
-  }
+		// pCont = new PController(leftMotor, rightMotor, 30, 2);
 
-  public boolean isRotating() {
-    return rotating;
-  }
+		leftMotor.setAcceleration(3000);
 
-  public boolean isGoingForward() {
-    return moving_forward;
-  }
+		rightMotor.setAcceleration(3000);
+
+	}
+	
+	public void moveForward(){
+		
+		leftMotor.setSpeed(ROTATE_SPEED);
+
+		rightMotor.setSpeed(ROTATE_SPEED);
+		
+		leftMotor.forward();
+		
+		rightMotor.forward();
+
+	}
+
+	public void rotate(double angle, boolean use_degrees, boolean ret) {
+
+		leftMotor.setAcceleration(1000);
+
+		rightMotor.setAcceleration(1000);
+
+		leftMotor.setSpeed(ROTATE_SPEED);
+
+		rightMotor.setSpeed(ROTATE_SPEED);
+
+		if (!use_degrees) {
+
+			angle = Math.toDegrees(angle);
+
+		}
+
+		if (angle < 0) {
+
+			// turn right
+
+			leftMotor.rotate(convertAngle(leftRadius, width, Math.abs(angle)), true);
+
+			rightMotor.rotate(-convertAngle(rightRadius, width, Math.abs(angle)), false || ret);
+
+		} else {
+
+			// turn left
+
+			leftMotor.rotate(-convertAngle(leftRadius, width, Math.abs(angle)), true);
+
+			rightMotor.rotate(convertAngle(rightRadius, width, Math.abs(angle)), false || ret);
+
+		}
+
+	}
+
+	public void moveTo(double dist, boolean ret) {
+
+		leftMotor.setAcceleration(3000);
+
+		rightMotor.setAcceleration(3000);
+
+		leftMotor.setSpeed(FORWARD_SPEED);
+
+		rightMotor.setSpeed(FORWARD_SPEED);
+
+		leftMotor.rotate(convertDistance(leftRadius, dist), true);
+
+		rightMotor.rotate(convertDistance(rightRadius, dist), false || ret);
+
+	}
+
+	public void stop() {
+
+		rightMotor.stop();
+
+		leftMotor.stop();
+
+	}
+
+	public void avoidObstacle(float dist) {
+
+		// pCont.processUSData(dist);
+
+	}
+
+	private static int convertDistance(double radius, double distance) {
+
+		return (int) ((180.0 * distance) / (Math.PI * radius));
+
+	}
+
+	private static int convertAngle(double radius, double width, double angle) {
+
+		return convertDistance(radius, Math.PI * width * angle / 360.0);
+
+	}
+
+	public boolean isRotating() {
+
+		return rotating;
+
+	}
+
+	public boolean isGoingForward() {
+
+		return moving_forward;
+
+	}
+
 }
