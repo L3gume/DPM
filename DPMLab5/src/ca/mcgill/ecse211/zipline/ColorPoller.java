7c57d14fc9752f12ba2581d4e8a9cdb5c18d379d
halestorm962 <a.hale@live.ca>
2017-10-23 15:23:08 -0400

    added comments

--- DPMLab5/src/ca/mcgill/ecse211/zipline/ColorPoller.java
+++ DPMLab5/src/ca/mcgill/ecse211/zipline/ColorPoller.java
@@ -3,14 +3,13 @@ package ca.mcgill.ecse211.zipline;
 import lejos.robotics.SampleProvider;
 
 /**
- * This class is very simple, its only function is to continuously check the light level.
+ * This class is very simple: its only function is to continuously check the light level.
  * 
  * @author Justin Tremblay
  *
- *         TODO: this class may need two color sensors, one for localizing and odometry correction
- *         and one for color detection
  */
 public class ColorPoller extends Thread {
+  // variables
   private SampleProvider sample1;
   private SampleProvider sample2;
   private float[] lightData1;
@@ -18,10 +17,10 @@ public class ColorPoller extends Thread {
   private LightLocalizer ll;
   private OdometryCorrection oc;
 
+  // modes
   public enum l_mode {
     NONE, LOCALIZATION, CORRECTION
   }
-
   private l_mode cur_mode = l_mode.NONE;
 
   /**
@@ -38,35 +37,43 @@ public class ColorPoller extends Thread {
     this.lightData2 = lightData2;
   }
 
+  /**
+   * Start the thread. Iterate forever, sending colour sensor readings to the classes that need them.
+   */
   public void run() {
-    while (true) {
-      sample2.fetchSample(lightData2, 0);
-      if (lightData2[0] > 0.f) {
-        oc.setLightLevel2(lightData2[0]);
-      }
-      if (cur_mode == l_mode.LOCALIZATION) {
-        sample1.fetchSample(lightData1, 0);
-        if (lightData1[0] > 0.f) {
-          ll.setLightLevel(lightData1[0]);
-        }
-        try {
-          Thread.sleep(20);
-        } catch (Exception e) {
-        } // Poor man's timed sampling
-      } else if (cur_mode == l_mode.CORRECTION){
-        // Nothing for now.
-      }
-    }
+	  // iterate forever
+	  while (true) {
+		  // send colour sensor reading to OdometryCorrection
+	      sample2.fetchSample(lightData2, 0);
+	      if (lightData2[0] > 0.f) {
+	        oc.setLightLevel2(lightData2[0]);
+	      }
+	      
+	      // if in localization mode, send colour sensor reading to LightLocalization
+	      if (cur_mode == l_mode.LOCALIZATION) {
+	        sample1.fetchSample(lightData1, 0);
+	        if (lightData1[0] > 0.f) {
+	          ll.setLightLevel(lightData1[0]);
+	        }
+	        try {
+	          Thread.sleep(20);
+	        } catch (Exception e) {
+	        } // Poor man's timed sampling
+	      } else if (cur_mode == l_mode.CORRECTION){
+	        // Nothing for now.
+	      }
+	  }
   }
 
+  /**
+   * Helper methods.
+   */
   public void setLocalizer(LightLocalizer ll) {
     this.ll = ll;
   }
-  
   public void setCorrection(OdometryCorrection oc) {
     this.oc = oc;
   }
-  
   public void setMode(l_mode localization) {
     cur_mode = localization;
   }
